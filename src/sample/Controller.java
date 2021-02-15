1da4549428f38b658c944d21a223eca7df7a70c0
Vijay Padmakumar <iamvijaypaddu@gmail.com>
2021-02-15 20:55:39 +0000

    hover property added to main screen button, and reorganised file structure

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,18 @@ import javafx.event.ActionEvent;
 public class Controller {
 
     public void playClicked(ActionEvent actionEvent) {
-        System.out.println("here");
+        System.out.println("Play clicked");
+    }
+
+    public void settingsClicked(ActionEvent actionEvent) {
+        System.out.println("Settings clicked");
+    }
+
+    public void controlsClicked(ActionEvent actionEvent) {
+        System.out.println("Controls clicked");
+    }
+
+    public void quitClicked(ActionEvent actionEvent) {
+        System.out.println("Quit clicked");
     }
 }
