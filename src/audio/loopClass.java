7782157d043e1ef347d85d92244623887808bf1a
lxm330 <798780038@qq.com>
2021-02-21 17:08:13 +0000

    n

---
+++ src/audio/loopClass.java
@@ -0,0 +1,20 @@
+package audio;
+
+import sun.audio.AudioPlayer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class loopClass {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        InputStream inputStream = new FileInputStream(new File("allMainPagesBgm.wav"));
+
+        musicStuff musicObject = new musicStuff();
+
+        AudioPlayer.player.start(inputStream);
+
+    }
+}
