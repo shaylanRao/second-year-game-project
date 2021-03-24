547f091c552935d6e9ea2a911771ede128b7e71f
Shaylan Rao <sxr980@student.bham.ac.uk>
2021-03-24 23:13:17 +0000

    Changed powerup button for player 1

--- src/sample/controllers/game/AbstractGameController.java
+++ src/sample/controllers/game/AbstractGameController.java
@@ -38,7 +38,7 @@ public abstract class AbstractGameController implements Initializable {
             case RIGHT:
                 this.getGame().getPlayerCar().setTurnRight(true);
                 break;
-            case L:
+            case CONTROL:
                 this.getGame().getPlayerCar().setActivatePowerup(true);
                 this.playerCar.setPickedUpPwrtime(System.currentTimeMillis());
                 break;
